a4a8ea8c1eb4cd594ec91e7d26a4ddfa5f80df8c
Vadim Korolik <vkorolik@gmail.com>
2022-07-14 16:50:00 -0700

    add automated graphql instrumentation

--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module github.com/highlight-run/highlight-go
 go 1.17
 
 require (
+	github.com/99designs/gqlgen v0.17.12
 	github.com/gin-gonic/gin v1.7.0
 	github.com/hasura/go-graphql-client v0.3.0
+	github.com/pkg/errors v0.9.1
 )
 
 require (
@@ -12,19 +14,20 @@ require (
 	github.com/go-playground/locales v0.13.0 // indirect
 	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/go-playground/validator/v10 v10.4.1 // indirect
-	github.com/golang/protobuf v1.3.5 // indirect
+	github.com/golang/protobuf v1.5.0 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/json-iterator/go v1.1.9 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/leodido/go-urn v1.2.0 // indirect
-	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
 	github.com/ugorji/go/codec v1.1.7 // indirect
-	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
-	golang.org/x/net v0.0.0-20211011170408-caeb26a5c8c0 // indirect
-	golang.org/x/sys v0.0.0-20210423082822-04245dca01da // indirect
-	gopkg.in/yaml.v2 v2.2.8 // indirect
+	github.com/vektah/gqlparser/v2 v2.4.6 // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f // indirect
+	golang.org/x/sys v0.0.0-20211019181941-9d821ace8654 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 	nhooyr.io/websocket v1.8.7 // indirect
 )
--- highlight_test.go
+++ highlight_test.go
@@ -3,6 +3,8 @@ package highlight
 import (
 	"context"
 	"fmt"
+	"github.com/99designs/gqlgen/graphql"
+	"github.com/vektah/gqlparser/v2/ast"
 	"strings"
 	"testing"
 
@@ -80,7 +82,7 @@ func TestRecordMetric(t *testing.T) {
 			RecordMetric(input.contextInput, input.metricInput.name, input.metricInput.value)
 			_, a := flush()
 			if len(a) != input.expectedFlushSize {
-				t.Errorf("flush returned the wrong number of errors [%v != %v]", len(a), input.expectedFlushSize)
+				t.Errorf("flush returned the wrong number of metrics [%v != %v]", len(a), input.expectedFlushSize)
 				return
 			}
 			if len(a) < 1 {
@@ -94,5 +96,43 @@ func TestRecordMetric(t *testing.T) {
 			}
 		})
 	}
+	Stop()
+}
+
+func TestTracer(t *testing.T) {
+	ctx := context.Background()
+	ctx = context.WithValue(ctx, ContextKeys.SessionSecureID, "0")
+	ctx = context.WithValue(ctx, ContextKeys.RequestID, "0")
+	ctx = graphql.WithOperationContext(ctx, &graphql.OperationContext{
+		Operation:     &ast.OperationDefinition{},
+		OperationName: "test-operation",
+		RawQuery:      "test-query",
+	})
+	ctx = graphql.WithFieldContext(ctx, &graphql.FieldContext{
+		Field: graphql.CollectedField{
+			Field: &ast.Field{Name: "test-field"},
+		},
+	})
+	tr := NewGraphqlTracer("test")
+	t.Run("test basic intercept", func(t *testing.T) {
+		Start()
+		if res := tr.InterceptResponse(ctx, func(ctx context.Context) *graphql.Response {
+			return graphql.ErrorResponse(ctx, "foo error")
+		}); res == nil {
+			t.Errorf("got invalid response from intercept response")
+		}
+		if field, err := tr.InterceptField(ctx, func(ctx context.Context) (res interface{}, err error) {
+			return &graphql.Response{}, nil
+		}); field == nil || err != nil {
+			t.Errorf("got invalid response from intercept field")
+		}
+
+		_, a := flush()
+		// size, duration, errorsCount, fields duration
+		if len(a) != 4 {
+			t.Errorf("flush returned the wrong number of metrics [%v != %v]", len(a), 4)
+			return
+		}
+	})
 	Stop()
 }
